bb477d710870b6194a61409e53f067e45a4e02f2
Bugaj <PIOTR_BUGAJ@homedepot.com>
2017-06-01 14:14:15 -0400

    Peter Bugaj: adding new code

--- src/main/scala/DataGenerator.scala
+++ src/main/scala/DataGenerator.scala
@@ -9,4 +9,19 @@ import org.apache.hadoop.conf.Configuration
   */
 object DataGenerator {
 
+  def main(args: Array[String]) {
+    
+    val conf : Configuration = HBaseConfiguration.create();
+    
+    val ZOOKEEPER_QUORUM =
+      "cdn-dataproc-custom-m" +
+      "cdn-dataproc-custom-w0" +
+      "cdn-dataproc-custom-w1";
+    println ("hbase.zookeeper.quorum" + ZOOKEEPER_QUORUM);
+    
+    conf.set("hbase.zookeeper.quorum", ZOOKEEPER_QUORUM);
+    
+    val admin = new HBaseAdmin(conf)
+
+  }
 }
